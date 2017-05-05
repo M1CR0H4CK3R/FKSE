82629c828b6969c2b1a333069bb6e7229f5ff1b1
Cuyler36 <jeremy96@vt.edu>
2017-05-05 19:40:33 -0400

    Rewrite Calculate_Checksum Method
    Now that I understand how the Checksum is calculated, I was able to
    greatly reduce the algorithm used to calculate it.

--- Classes/Checksum.cs
+++ Classes/Checksum.cs
@@ -13,41 +13,17 @@ namespace FKSE
         {
             if (Save_Buffer.Length == 0x1160)
             {
-                ushort Checksum = 0; // Checksum = Z (r0)
-                ushort Counter = 0; // C (r5)
-                ushort X = 0, Y = 0;
+                ushort Checksum = 0;
+                ushort Last_WORD = 0;
 
-                for (int i = 0; i < Save_Buffer.Length; i += 16) //Address = r9
+                for (int i = 0; i < 0x8B0; i++)
                 {
-                    for (int sub = 0; sub < 8; sub++)
-                    {
-                        int Sub_Offset = i + sub * 2; //Address + 2
-                        ushort Current_WORD = (ushort)((Save_Buffer[Sub_Offset] << 8) + Save_Buffer[Sub_Offset + 1]); //GC Has Reverse Endianness (Big Endian)
-                        if (sub == 0)
-                        {
-                            //Step 3
-                            Y = Current_WORD;
-                            Counter += 8; //Step 4
-                        }
-                        else if (sub % 2 == 1)
-                        {
-                            //Steps 5 -> 6, 9 -> 10, 13 -> 14, 17 -> 18
-                            X = Current_WORD;
-                            Checksum ^= Y;
-                        }
-                        else
-                        {
-                            //Steps 7 -> 8, 11 -> 12, 15 -> 16
-                            Y = Current_WORD;
-                            Checksum ^= X;
-                        }
-                    }
-                    Checksum ^= X; //Step 20
-                                   //Checksum &= 0xFFFF; // This is implied when it overflows
-                    if (Counter > 0x08A8) //Step 21
-                        break;
+                    if (i > 0)
+                        Checksum ^= Last_WORD;
+                    Last_WORD = (ushort)((Save_Buffer[i * 2] << 8) + Save_Buffer[i * 2 + 1]);
                 }
-                return (ushort)(Checksum ^ 0x4C6B);
+                Checksum ^= Last_WORD;
+                return (ushort)(Checksum ^ 0x4C6B); //0x4C6B is the XOR key used for obfuscation
             }
             throw (new ArgumentException(string.Format("Checksum Calculator was passed a byte array with an invalid Length. Expected a length of 0x1160, but got length: 0x{0}", Save_Buffer.Length.ToString("X"))));
         }
