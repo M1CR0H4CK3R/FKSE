5f431464f84ed12a89e0613461201d76956e3390
M1CR0H4CK3R <cartermarcus04@yahoo.com>
2017-05-04 20:23:25 -0400

    Initial Commit

---
+++ Classes/Save.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace FKSE
+{
+    public enum SaveType
+    {
+        Unknown,
+        TFK_USA
+    }
+
+    public struct Offsets
+    {
+        public int ItemData;
+        public int ItemDataSize;
+        public int Money;
+        public int StoryData;
+        public int StoryDataSize;
+        public int MonsterData;
+        public int MonsterDataSize;
+        public int CharacterData;
+        public int CharacterDataSize;
+    }
+
+    public enum SaveFileDataOffset
+    {
+        gyfegci = 0x4440,
+        gyfegcs = 0x4550
+    }
+
+    public static class SaveDataManager
+    {
+        public static SaveType GetSaveType(byte[] Save_Data)
+        {
+            if (Save_Data.Length == 0x6040 || Save_Data.Length == 0x6150)
+            {
+                string Game_ID = Encoding.ASCII.GetString(Save_Data, Save_Data.Length == 0x6150 ? 0x110 : 0, 4);
+                if (Game_ID == "GYFE")
+                    return SaveType.TFK_USA;
+            }
+            return SaveType.Unknown;
+        }
+
+        public static int GetSaveDataOffset(string Game_ID, string Extension)
+        {
+            SaveFileDataOffset Extension_Enum;
+            if (Enum.TryParse(Game_ID + Extension, out Extension_Enum))
+                return (int)Extension_Enum;
+            return 0;
+        }
+
+        public static string GetGameID(SaveType Save_Type)
+        {
+            switch (Save_Type)
+            {
+                case SaveType.TFK_USA:
+                    return "GYFE";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+
+    //Note 1
+    //Note 2
+
+    public class Save
+    {
+        public SaveType Save_Type;
+        public byte[] Working_Save_Data;
+        public int[] Save_Offsets;
+        public int Save_Data_Start_Offset;
+        public string Save_Path;
+        public string Save_Name;
+        public string Save_Extension;
+        public string Save_ID;
+        public bool Is_Big_Endian = true;
+        private FileStream Save_File;
+        private BinaryReader Save_Reader;
+        private BinaryWriter Save_Writer;
+
+        public Save(string File_Path)
+        {
+            if (File.Exists(File_Path))
+            {
+                if (Save_File != null)
+                {
+                    Save_Reader.Close();
+                    Save_File.Close();
+                }
+                Save_File = new FileStream(File_Path, FileMode.Open);
+                if (!Save_File.CanWrite)
+                {
+                    MessageBox.Show(string.Format("Error: File {0} is being used by another process. Please close any process using it before editing!",
+                        Path.GetFileName(File_Path)), "File Opening Error");
+                    try { Save_File.Close(); }
+                    catch { };
+                    return;
+                }
+
+                Save_Reader = new BinaryReader(Save_File);
+
+                Working_Save_Data = new byte[Save_File.Length];
+
+                Save_Type = SaveDataManager.GetSaveType(Working_Save_Data);
+                Save_Name = Path.GetFileNameWithoutExtension(File_Path);
+                Save_Path = Path.GetDirectoryName(File_Path) + Path.DirectorySeparatorChar;
+                Save_Extension = Path.GetExtension(File_Path);
+                Save_ID = SaveDataManager.GetGameID(Save_Type);
+                Save_Data_Start_Offset = SaveDataManager.GetSaveDataOffset(Save_ID.ToLower(), Save_Extension.Replace(".", "").ToLower());
+
+                Save_Reader.Close();
+                Save_File.Close();
+            }
+        }
+        public void Flush()
+        {
+            string Full_Save_Name = Save_Path + Path.DirectorySeparatorChar + Save_Name + Save_Extension;
+            Save_File = new FileStream(Full_Save_Name, FileMode.OpenOrCreate);
+            Save_Writer = new BinaryWriter(Save_File);
+
+            Save_Writer.Write(Working_Save_Data);
+            Save_Writer.Flush();
+            Save_File.Flush();
+
+            Save_Writer.Close();
+            Save_File.Close();
+        }
+
+        public void Write(int offset, object data, bool reversed = false)
+        {
+            Type Data_Type = data.GetType();
+            if (!Data_Type.IsArray)
+            {
+                if (Data_Type == typeof(byte))
+                    Working_Save_Data[offset] = (byte)data;
+                else
+                {
+                    byte[] Byte_Array = BitConverter.GetBytes((dynamic)data);
+                    if (reversed)
+                        Array.Reverse(Byte_Array);
+                    Buffer.BlockCopy(Byte_Array, 0, Working_Save_Data, offset, Byte_Array.Length);
+                }
+            }
+            else
+            {
+                dynamic Data_Array = (dynamic)data;
+                if (Data_Type == typeof(byte[]))
+                    for (int i = 0; i < Data_Array.Length; i++)
+                        Working_Save_Data[offset + i] = Data_Array[i];
+                else
+                {
+                    int Data_Size = Marshal.SizeOf(Data_Array[0]);
+                    for (int i = 0; i < Data_Array.Length; i++)
+                    {
+                        byte[] Byte_Array = BitConverter.GetBytes(Data_Array[i]);
+                        if (reversed)
+                            Array.Reverse(Byte_Array);
+                        Byte_Array.CopyTo(Working_Save_Data, offset + i * Data_Size);
+                    }
+                }
+            }
+        }
+
+        public byte ReadByte(int offset)
+        {
+            return Working_Save_Data[offset];
+        }
+
+        public byte[] ReadByteArray(int offset, int count, bool reversed = false)
+        {
+            byte[] Data = new byte[count];
+            Buffer.BlockCopy(Working_Save_Data, offset, Data, 0, count);
+            if (reversed)
+                Array.Reverse(Data);
+            return Data;
+        }
+
+        public ushort ReadUInt16(int offset, bool reversed = false)
+        {
+            return BitConverter.ToUInt16(ReadByteArray(offset, 2, reversed), 0);
+        }
+
+        public ushort[] ReadUInt16Array(int offset, int count, bool reversed = false)
+        {
+            ushort[] Returned_Values = new ushort[count];
+            for (int i = 0; i < count; i++)
+                Returned_Values[i] = ReadUInt16(offset + i * 2, reversed);
+            return Returned_Values;
+        }
+
+        public uint ReadUInt32(int offset, bool reversed = false)
+        {
+            return BitConverter.ToUInt32(ReadByteArray(offset, 4, reversed), 0);
+        }
+
+        public uint[] ReadUInt32Array(int offset, int count, bool reversed = false)
+        {
+            uint[] Returned_Values = new uint[count];
+            for (int i = 0; i < count; i++)
+                Returned_Values[i] = ReadUInt32(offset + i * 4, reversed);
+            return Returned_Values;
+        }
+    }
+}
