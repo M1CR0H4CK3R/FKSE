d7c4671f9177fc4263dc62cee31978797e100fd8
Cuyler36 <jeremy96@vt.edu>
2017-05-06 00:22:18 -0400

    Implement Open/Save File and Gold Editing
    Title explains all.

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -15,19 +15,16 @@ namespace FKSE
 {
     public partial class MainForm : Form
     {
-        public static byte[] saveBuffer = new byte[0x4440];
-
         #region Offsets
-        public static int DataStart_Offset = 0x4440;
-        public static int ItemData_Offset = 0x50;
-        public static int ItemData_Size = 0x200;
-        public static int Money_Offset = 0x250;
-        public static int Money_Size = 0x4;
-        public static int StoryData_Offset = 0x254;
+        //TODO: Move this code out of this form and into its own class. (Also updated the rest to correct values)
+        public static int ItemData_Offset = 0x250;
+        public static int ItemData_Size = 0x400;
+        public static int Money_Offset = 0x4690;
+        public static int StoryData_Offset = 0x454;
         public static int StoryData_Size = 0xC;
-        public static int MonsterData_Offset = 0x260;
+        public static int MonsterData_Offset = 0x460;
         public static int MonsterData_Size = 0xDD4;
-        public static int CharacterData_Offset = 0x1034;
+        public static int CharacterData_Offset = 0x1234;
         public static int CharacterData_Size = 0x118;
         #endregion Offsets
 
@@ -35,16 +32,12 @@ namespace FKSE
         public static readonly string Assembly_Location = Directory.GetCurrentDirectory();
         static readonly DateTime _unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         //static readonly int Date_Offset = 946684799; //Date at 12/31/1999 @ 11:59:59PM
-        private FileStream fs;
-        private BinaryReader reader;
-        private BinaryWriter writer;
-        public static string fileName;
-        private ItemEditor itemEditorForm;
-        private MonsterEditor monsterEditorForm;
-        private CharacterEditor characterEditorForm;
-        private CharacterMonsterEditor charMonsterEditorForm;
-        private CancelEventHandler importHandler;
-        public static byte[] SaveBuffer { get { return saveBuffer; } }
+        //private ItemEditor itemEditorForm;
+        //private MonsterEditor monsterEditorForm;
+        //private CharacterEditor characterEditorForm;
+        //private CharacterMonsterEditor charMonsterEditorForm;
+        //private CancelEventHandler importHandler;
+        private Save Save_File;
         #endregion Variables
 
         public static DateTime DateFromTimestamp(long timestamp)
@@ -52,30 +45,39 @@ namespace FKSE
             return _unixEpoch.AddSeconds(timestamp);
         }
 
-        private byte[] GetSaveData(int offset, int size)
-        {
-            reader.BaseStream.Seek(offset, SeekOrigin.Begin);
-            return reader.ReadBytes(size);
-        }
-
-        private void SaveData()
-        {
-            Checksum.Update(saveBuffer);
-            fs = new FileStream(fileName, FileMode.Open);
-            writer = new BinaryWriter(fs);
-            writer.Seek(Data_Start_Offset, SeekOrigin.Begin);
-            writer.Write(saveBuffer);
-            writer.Close();
-            fs.Close();
-        }
-
-
         public MainForm()
         {
             InitializeComponent();
-            ItemData.SetupItemDictionary();
-            MonsterData.SetupMonsterDictionary();
-            CharacterData.SetupCharacterDictionary();
+            //ItemData.SetupItemDictionary();
+            //MonsterData.SetupMonsterDictionary();
+            //CharacterData.SetupCharacterDictionary();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Save_File = new Save(openFileDialog1.FileName);
+                //TODO: Implement reading method more elegantly
+                goldTextBox.Text = Save_File.ReadUInt32(Save_File.Save_Data_Start_Offset + Money_Offset, true).ToString();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Save_File != null)
+            {
+                Checksum.Update_Checksum(Save_File);
+                Save_File.Flush();
+            }
+        }
+
+        private void goldTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (Save_File != null && uint.TryParse(goldTextBox.Text, out uint New_Gold_Value))
+            {
+                Save_File.Write(Save_File.Save_Data_Start_Offset + Money_Offset, New_Gold_Value, true); //TODO: Change this to use the out of form gold offset
+            }
         }
     }
 }
